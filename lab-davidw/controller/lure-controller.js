452d25803c3b0dc123c53faccdf3ecf8949e75e3
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-05-01 19:07:01 -0700

    add scaffolded project

---
+++ lab-davidw/controller/lure-controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+const createError = require('http-errors');
+
+module.exports = exports = {};
+
+exports.createItem = function() {
+
+};
+
+exports.fetchItem = function() {
+
+};
